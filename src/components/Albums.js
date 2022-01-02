487298cf32bf33624b975304d840a50a6e406014
Tom Officer <thomasofficerjr@gmail.com>
2022-01-02 01:30:34 -0800

    added state and click handlers

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -7,26 +7,50 @@ import Plausibility from './Plausibility';
 
 const Albums = () => {
   //state variables
-  const [showSlaughterhaus, setShowSlaughterhaus] = useState(false);
-  const [showPlausibility, setShowPlausibility] = useState(false);
+  const [showAlbum1, setShowAlbum1] = useState(false);
+  const [showAlbum2, setShowAlbum2] = useState(false);
+  const [showAlbum3, setShowAlbum3] = useState(false);
+  const [showAlbum4, setShowAlbum4] = useState(false);
+  const [showAlbum5, setShowAlbum5] = useState(false);
+  const [showAlbum6, setShowAlbum6] = useState(false);
 
   //click handlers
-  const slaughterhausClick = () => {
-    setShowSlaughterhaus(!showSlaughterhaus);
+  const album1Click = () => {
+    setShowAlbum1(!showAlbum1);
   };
 
-  const plausibilityClick = () => {
-    setShowPlausibility(!showPlausibility);
+  const album2Click = () => {
+    setShowAlbum2(!showAlbum2);
   };
+
+  const album3Click = () => {
+    setShowAlbum3(!showAlbum3);
+  };
+
+  const album4Click = () => {
+    setShowAlbum4(!showAlbum4);
+  };
+
+  const album5Click = () => {
+    setShowAlbum5(!showAlbum5);
+  };
+
+  const album6Click = () => {
+    setShowAlbum6(!showAlbum6);
+  };
+
+  //work on logic to shorten the click handlers and state...
+  //use effect section
+  //implement dynamic logic so each time an album is clicked or state changes, render new component
 
   return (
     <div>
       <div className='albums__container'>
         <div className='albums__album-item'>
-          <img src={slaughterhaus} alt='' onClick={slaughterhausClick} />
+          <img src={slaughterhaus} alt='' onClick={album1Click} />
         </div>
         <div className='albums__album-item'>
-          <img src={plausibility} alt='' onClick={plausibilityClick} />
+          <img src={plausibility} alt='' onClick={album2Click} />
         </div>
 
         {showSlaughterhaus && <Slaughterhaus />}
